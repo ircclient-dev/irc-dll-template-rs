1198ba58327a91451e2feb09d6423a670812ad55
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-02-10 19:30:37 +1100

    feat: implement thread-safe LOADINFO with OnceLock and enhance error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::ptr;
+use std::sync::OnceLock;
 use std::time::Duration;
 use windows::core::{w, PCWSTR};
 use windows::Win32::Foundation::{BOOL, HWND};
 
 #[repr(C)]
+#[derive(Clone, Debug)]
 pub struct LOADINFO {
     // m_version contains the mIRC version number (AdiIRC uses a mIRC compatibility version number) in the low and high words.
     m_version: u32,
@@ -21,6 +23,12 @@ pub struct LOADINFO {
     m_bytes: u32,
 }
 
+// Implement Sync and Send for LOADINFO
+unsafe impl Sync for LOADINFO {}
+unsafe impl Send for LOADINFO {}
+
+static LOADINFO: OnceLock<LOADINFO> = OnceLock::new();
+
 #[no_mangle]
 pub extern "system" fn hello(
     _m_wnd: HWND,
@@ -32,26 +40,37 @@ pub extern "system" fn hello(
 ) -> i32 {
     let result = w!("Hello, world!");
 
-    unsafe {
-        // This is "Unsafe" as we're going to write to memory our process doesn't own (AdiIRC/mIRC owns it)
+    // Obtain the LOADINFO that we stored during the LoadDll call
+    let loadinfo = LOADINFO.get();
+    match loadinfo {
+        Some(_loadinfo) => {
+            unsafe {
+                // This is "Unsafe" as we're going to write to memory our process doesn't own (AdiIRC/mIRC owns it)
 
-        // Fix for https://dev.adiirc.com/issues/5808 (AdiIRC doesn't stop reading after a null terminator)
-        if data.len() > result.len() {
-            ptr::write_bytes(
-                data.0.offset(result.len() as isize + 1) as *mut u16,
-                0,
-                data.len() - result.len() - 1,
-            );
+                // Write the result to the data variable
+                // TODO: We should make sure that the data variable is large enough to hold the result (by checking LOADINFO.m_bytes)
+                ptr::copy_nonoverlapping(result.as_ptr(), data.0 as *mut u16, result.len() + 1);
+
+                // Fix for https://dev.adiirc.com/issues/5808 (AdiIRC doesn't stop reading after a null terminator)
+                if data.len() > result.len() {
+                    ptr::write_bytes(
+                        data.0.offset(result.len() as isize + 1) as *mut u16,
+                        0,
+                        data.len() - result.len() - 1,
+                    );
+                }
+            }
+            3 // The return value can be:
+              // - 0 means that mIRC should /halt processing
+              // - 1 means that mIRC should continue processing
+              // - 2 means that it has filled the data variable with a command which it wants mIRC to perform, and has filled parms with the parameters to use, if any, when performing the command.
+              // - 3 means that the DLL has filled the data variable with the result that $dll() as an identifier should return.
         }
-
-        // Write the result to the data variable
-        ptr::copy_nonoverlapping(result.as_ptr(), data.0 as *mut u16, result.len() + 1);
+        None => {
+            eprintln!("Error: LOADINFO not initialised");
+            return 0;
+        }
     }
-    3 // The return value can be:
-      // - 0 means that mIRC should /halt processing
-      // - 1 means that mIRC should continue processing
-      // - 2 means that it has filled the data variable with a command which it wants mIRC to perform, and has filled parms with the parameters to use, if any, when performing the command.
-      // - 3 means that the DLL has filled the data variable with the result that $dll() as an identifier should return.
 }
 
 #[no_mangle]
@@ -72,9 +91,18 @@ pub extern "system" fn LoadDll(info: *mut LOADINFO) {
             .unwrap();
         std::thread::sleep(Duration::from_millis(1000)); // Wait for debugger to attach
     }
+
     unsafe {
+        // Set mKeep and mUnicode to TRUE by default
         (*info).m_keep = BOOL(1); // Keep the DLL loaded
         (*info).m_unicode = BOOL(1); // Use Unicode
+
+        // Store (a copy of) LOADINFO for later use.
+        // - It's not clear from documentation if this memory will be available outside of LoadDll, so we assume it won't be.
+        let result = LOADINFO.set((*info).clone());
+        if let Err(e) = result {
+            eprintln!("Error: LOADINFO already initialised: {:?}", e);
+        }
     }
 }
 
@@ -96,6 +124,16 @@ mod tests {
 
     #[test]
     fn test_hello() {
+        let mut loadinfo = LOADINFO {
+            m_version: 0,
+            m_hwnd: HWND(std::ptr::null_mut()),
+            m_keep: BOOL(0),
+            m_unicode: BOOL(0),
+            m_beta: 0,
+            m_bytes: 0,
+        };
+        LoadDll(&mut loadinfo);
+
         let m_wnd: HWND = HWND(std::ptr::null_mut());
         let a_wnd: HWND = HWND(std::ptr::null_mut());
         let data_str = "The quick brown fox jumps over the lazy dog";
@@ -106,6 +144,7 @@ mod tests {
         let nopause: BOOL = BOOL(0);
 
         let result = hello(m_wnd, a_wnd, data, parms, show, nopause);
+        unsafe { println!("Result: {:?}", data.to_string()); }
         unsafe { assert_eq!(data.as_wide(), w!("Hello, world!").as_wide()) };
         assert_eq!(result, 3);
     }
